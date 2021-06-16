49924ac84cf78dac657eea301d768e57c5f93e49
chriscauley <chris@lablackey.com>
2021-06-16 03:21:05 +0000

    updating @unrest/vue

--- client/src/main.js
+++ client/src/main.js
@@ -1,13 +1,16 @@
-import auth from '@unrest/vue-auth'
-import UrVue, { ui } from '@unrest/vue'
+import auth from "@unrest/vue-auth";
+import UrVue from "@unrest/vue";
+import form from "@unrest/form";
+import "@unrest/tailwind/dist.css";
 
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
 
 createApp(App)
   .use(router)
+  .use(form.plugin)
   .use(auth.plugin)
-  .use(UrVue)
-  .use(ui)
-  .mount('#app')
+  .use(UrVue.plugin)
+  .use(UrVue.ui)
+  .mount("#app");
--- client/src/router.js
+++ client/src/router.js
@@ -1,20 +1,19 @@
-import { createRouter, createWebHistory, createMemoryHistory } from 'vue-router';
-import { loadViews, applyMeta } from '@unrest/vue';
-import auth from '@unrest/vue-auth';
-import views from './views';
+import { createRouter, createWebHistory } from "vue-router";
+import Unrest from "@unrest/vue";
+import auth from "@unrest/vue-auth";
+import views from "./views";
 
-const routes = [
-  ...auth.routes,
-  ...loadViews(views),
-];
+const { loadViews, applyMeta } = Unrest;
+
+const routes = [...auth.routes, ...loadViews(views)];
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
-})
+});
 
-router.beforeEach(applyMeta)
+router.beforeEach(applyMeta);
 router.beforeEach(() => {
-  // refresh any api calls after navigation 
-})
-export default router
+  // refresh any api calls after navigation
+});
+export default router;
