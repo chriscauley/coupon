a6209d20eb0bb0856bbf8786d745a0668e6668ef
chriscauley <chris@lablackey.com>
2021-06-18 12:13:59 +0000

    moving some python files into server

--- server/manage.py
+++ server/manage.py
@@ -6,7 +6,7 @@ from django.core.management.commands.runserver import Command as runserver
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
